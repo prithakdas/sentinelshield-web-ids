1b10c29323963c562b238d3f75cd6b7bb9d5f9f6
prithakdas <prithakpdas@gmail>
2026-03-15 17:44:00 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -197,4 +197,5 @@ def dashboard():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False)
